0c7723c69de77cc97d70583c8cd53f595b2914d7
chengzhx76 <chengzhx76@qq.com>
2017-08-15 14:21:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+##微信商城
+
+基于原来[Dubbo](https://github.com/chengzhx76/Weixin-Dubbo)项目改造成SpringCloud项目，欢迎Star
+
+
---
+++ shop-service-user/src/main/java/com/github/chengzhx76/service/user/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.github.chengzhx76.service.user;
+
+/**
+ * Desc:
+ * Author: chengzhx76@qq.com
+ * Date: 2017/8/15
+ */
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
