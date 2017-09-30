91df79b57d38571e6e1232fe8179673725d52017
chengzhx76 <chengzhx76@qq.com>
2017-09-30 11:16:20 +0800

    增加注册中心

---
+++ shop-service-base/shop-eureka-server/src/main/java/com/github/chengzhx76/base/server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.github.chengzhx76.base.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Desc:
+ * Author: chengzhx76@qq.com
+ * Date: 2017/7/17
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
